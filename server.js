f4b11394759fa203be7322b91a5853ca2e16ec86
claudiauhr <claudia.c.uhr@gmail.com>
2022-12-12 18:13:28 -0500

    wip: adding data model/drinks.js server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express")
+const { read } = require("fs")
+const drinks = require("./models/drinks.js")
+
 const app = express()
 const port = 3000
 
@@ -6,6 +9,9 @@ app.get("/",(req, res) => {
     res.send('Welcome to the Github App!')
 })
 
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
+})
 app.listen(port, () => {
     console.log("Server is listening.")
 })
